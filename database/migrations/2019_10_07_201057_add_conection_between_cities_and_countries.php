baba22bdf42d89320d6d5ebd3426c6de1d0bc337
Alexey Rodin <razraz.odinodin@gmail.com>
2019-10-07 20:26:42 +0300

    create base tables

---
+++ database/migrations/2019_10_07_201057_add_conection_between_cities_and_countries.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddConectionBetweenCitiesAndCountries extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('cities', function (Blueprint $table) {
+            $table->unsignedBigInteger('country_id');
+
+            $table->foreign('country_id')
+                ->references('id')
+                ->on('countries');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('cities', function (Blueprint $table) {
+            $table->dropForeign(['country_id']);
+            $table->dropColumn('country_id');
+        });
+    }
+}
