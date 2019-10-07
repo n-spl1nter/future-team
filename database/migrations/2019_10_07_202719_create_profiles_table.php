cf93402dcec1ed9f13bb130b9b4f970895452c25
Alexey Rodin <razraz.odinodin@gmail.com>
2019-10-07 21:53:57 +0300

    add lang tables

---
+++ database/migrations/2019_10_07_202719_create_profiles_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('last_name');
+            $table->string('phone');
+            $table->string('photo');
+            $table->smallInteger('age');
+            $table->smallInteger('language_exchange_agreement')->default(0);
+            $table->unsignedBigInteger('city_id');
+            $table->unsignedBigInteger('activity_field_id');
+            $table->text('motivation');
+            $table->timestamps();
+
+            $table->foreign('city_id')
+                ->references('id')
+                ->on('cities');
+            $table->foreign('activity_field_id')
+                ->references('id')
+                ->on('activity_fields');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            $table->dropForeign(['city_id']);
+            $table->dropForeign(['activity_fields']);
+        });
+        Schema::dropIfExists('profiles');
+    }
+}
---
+++ database/migrations/2019_10_07_215301_languages__w_l_t_l__table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class LanguagesWLTLTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('languages_wltl', function (Blueprint $table) {
+
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('language_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->foreign('language_id')
+                ->references('id')
+                ->on('languages');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('languages_wltl', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['language_id']);
+        });
+        Schema::dropIfExists('languages_wltl');
+    }
+}
