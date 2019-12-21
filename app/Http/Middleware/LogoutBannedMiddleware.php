04ef891bd3d34a487042ace10601fd363a68f3e5
Alexey Rodin <razraz.odinodin@gmail.com>
2019-12-21 13:57:00 +0300

    revoke tokens in middleware

--- app/Http/Middleware/LogoutBannedMiddleware.php
+++ app/Http/Middleware/LogoutBannedMiddleware.php
@@ -4,22 +4,25 @@ namespace App\Http\Middleware;
 
 use App\Entities\User;
 use Closure;
+use Illuminate\Auth\AuthenticationException;
 
 class LogoutBannedMiddleware
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
+     * @throws AuthenticationException
      */
     public function handle($request, Closure $next)
     {
         /** @var User|null $user */
         $user = $request->user();
         if ($user && $user->isBlocked()) {
-            \Auth::logout();
+            $user->token()->revoke();
+            throw new AuthenticationException();
         }
         return $next($request);
     }
